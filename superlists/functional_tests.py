d1b0a9738490f5af0b17675eba4b6e2dc740ed1d
ZhenyuanLau <zhenyuanlau@icloud.com>
2018-06-30 12:03:02 +0800

    Finished function tests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -17,16 +17,31 @@ class VisitorTest(unittest.TestCase):
 
         # 看到网页标题包含 “To-Do”
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finished the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # 请输入一个代办事项
+        inputbox = self.browser.find_element_by_id('id-new-item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
-        # 输入 参加大学同学聚会
+        # 输入 Learning
+        inputbox.send_keys('Learning')
 
-        # 回车，页面更新
-        # 页面显示 1. 参加大学同学聚会
+        # 回车
+        inputbox.send_keys(Keys.ENTER)
+
+        # 页面显示 1. Learning
+        table = self.browser.find_element_by_id('id-list-table')
+        rows = self.browser.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1. Learning' for row in rows)
+        )
 
         # 退出
+        self.fail('Finished the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings = 'ignore')
