59a7bc339e93309c38a2d3d38f535d7ed926ddf1
ZhenyuanLau <zhenyuanlau@icloud.com>
2018-07-01 12:35:42 +0800

    Creating List

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page, view_list
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
@@ -12,15 +12,23 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'The second list item'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -29,7 +37,9 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
 
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'The second list item')
+        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
 
@@ -38,8 +48,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, List
 
 # 编写视图
 def home_page(request):
@@ -10,6 +10,7 @@ def view_list(request):
     return render(request, 'list.html', { 'items': items })
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item-text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item-text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
     return redirect('/lists/the-only-list-in-the-world/')
